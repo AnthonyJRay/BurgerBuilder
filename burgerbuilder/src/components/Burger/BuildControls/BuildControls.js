58eb896ba03978213914b2923d119f18d8c9608e
Anthony Eriksen <ajrayid@gmail.com>
2020-01-15 10:32:48 -0700

    Added Ingredient Buttons, Added Order button, disabled functionality.

--- burgerbuilder/src/components/Burger/BuildControls/BuildControls.js
+++ burgerbuilder/src/components/Burger/BuildControls/BuildControls.js
@@ -10,12 +10,24 @@ const controls = [
   { label: 'Meat', type: 'meat' }
 ];
 
-const buildControls = props => {
+const buildControls = props => (
   <div className={classes.BuildControls}>
+    <p>
+      Current Price: <strong>{props.price.toFixed(2)}</strong>
+    </p>
     {controls.map(ctrl => (
-      <BuildControl key={ctrl.label} label={ctrl.label} />
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemove(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
     ))}
-  </div>;
-};
+    <button className={classes.OrderButton} disabled={!props.purchasable}>
+      ORDER NOW
+    </button>
+  </div>
+);
 
 export default buildControls;
---
+++ burgerbuilder/src/components/UI/Modal/Modal.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const modal = props => <div>{props.children}</div>;
+
+export default modal;
