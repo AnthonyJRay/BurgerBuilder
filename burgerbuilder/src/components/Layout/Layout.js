efecf42c95927164bae57e667d6dc07103af95b2
Anthony Eriksen <ajrayid@gmail.com>
2020-01-15 19:59:10 -0700

    Added SideDrawer component

--- burgerbuilder/src/components/Layout/Layout.js
+++ burgerbuilder/src/components/Layout/Layout.js
@@ -3,17 +3,15 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-// prettier-ignore
 const layout = props => {
   return (
     <Aux>
       <Toolbar />
+      <SideDrawer />
       <div>Toolbar, SideDrawer, Backdrop</div>
-      <main 
-        className={classes.Content}>
-        {props.children}
-      </main>
+      <main className={classes.Content}>{props.children}</main>
     </Aux>
   );
 };
